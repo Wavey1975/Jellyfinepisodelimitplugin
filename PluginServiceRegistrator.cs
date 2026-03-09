1616ed75c3977132a240b7b716163e3b231ee520
Wavey1975 <davey_steve@hotmail.com>
2026-03-09 14:54:00 +0000

    PluginServiceRegistrator.cs

--- PluginServiceRegistrator.cs
+++ PluginServiceRegistrator.cs
@@ -5,16 +5,12 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Jellyfin.Plugin.EpisodeLimit;
 
-/// <summary>
-/// Register plugin services with the DI container.
-/// </summary>
 public class PluginServiceRegistrator : IPluginServiceRegistrator
 {
-    /// <inheritdoc />
     public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
     {
-        // Register PlaybackLimitService as singleton so API controller and hosted service share the same instance
         serviceCollection.AddSingleton<PlaybackLimitService>();
         serviceCollection.AddHostedService(sp => sp.GetRequiredService<PlaybackLimitService>());
+        serviceCollection.AddHostedService<ScriptInjectorService>();
     }
 }
